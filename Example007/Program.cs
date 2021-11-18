24c747dde56f62bc1228115ff734e3f3ede6b678
BorisBritv <malcoach@mail.ru>
2021-11-18 20:56:56 +0300

    add 007

---
+++ Example007/Program.cs
@@ -0,0 +1,25 @@
+﻿// 7. Показать числа от -N до N
+
+int[] A(int N)                                                // метод по созданию нового массива и заполнению его значениями 
+{
+    int[] arr = new int[N];                                   // создал новый массив
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-5,7);  // заполнил массив случайными значениями от минимального до максимального
+    }
+    return arr;
+}
+
+
+
+string PrintArray(int[]arr)                                    // метод с типом данных string в который приходит массив
+{
+    string res = string.Empty;                               
+    for (int i = 0; i <arr.Length; i++)
+    {
+        res+=arr[i];                                           //  в res складываю значения массива который создавал в методе выше
+    }
+    return res;
+}
+int[] newArr= A(8);                                            // создал новый массив чтобы положить в него метод с длинной массива
+Console.Write(PrintArray(newArr));
