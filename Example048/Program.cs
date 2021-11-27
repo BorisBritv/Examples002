43a9571a4e80565c575566d9b88209451aa715f8
BorisBritv <malcoach@mail.ru>
2021-11-27 19:42:51 +0300

    add 048

---
+++ Example048/Program.cs
@@ -0,0 +1,42 @@
+﻿// 48 Показать двумерный массив размером m×n заполненный целыми числами
+
+
+
+void FillArray(int[,] array)                                           // метод заполнения массива
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 11);
+        }
+    }
+
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+     
+}
+
+
+int m=6;
+int n = 5;
+
+int[,] array = new int [ m, n ];
+
+
+
+FillArray(array);
+
+PrintArray(array);
