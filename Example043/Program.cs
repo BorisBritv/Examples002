7101fb2eca34546c01904c8a0d706f4839a1b2e2
BorisBritv <malcoach@mail.ru>
2021-12-02 11:10:13 +0300

    add 043

---
+++ Example043/Program.cs
@@ -0,0 +1,16 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное
+
+double A(int N)
+{
+    int mod = 0;
+    double d = 0;
+    for (int i = 0; N > 0; i++)
+    {
+        mod = N % 2;                                 // oстаток от деления
+        N = (N - mod) / 2;                           // 
+        d += mod * Math.Pow(10, i);                  
+    }
+    return d;
+}
+
+Console.WriteLine(A(2));
