3f320c797f38df042342b46ce171e35d6939bd5b
BorisBritv <malcoach@mail.ru>
2021-12-02 10:43:34 +0300

    add 042

---
+++ Example042/Program.cs
@@ -0,0 +1,21 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Write("Введите число ");
+
+int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+
+int A(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+Console.WriteLine(A(array));
+
