0fee6f82ca4b552507febd820b2cdc9c059ad559
BorisBritv <malcoach@mail.ru>
2021-12-04 17:33:30 +0300

    add 066

---
+++ Example066/Program.cs
@@ -0,0 +1,17 @@
+﻿//66.Показать натуральные числа от 1 до N, N задано
+
+string A(int N)
+{
+    if (N == 1) return "1 ";
+    else return $"{(A(N - 1) + N)} ";
+}
+    Console.WriteLine(A(8));
+
+    
+// int N = 8;
+// for (int i = 1; i < N; i++)
+// {
+
+// }
+
+
