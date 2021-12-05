b1c70b58a506e22706c717a8f16a14db2bd70d8a
BorisBritv <malcoach@mail.ru>
2021-12-05 13:12:09 +0300

    add 068 new

--- Example068/Program.cs
+++ Example068/Program.cs
@@ -1,9 +1,20 @@
 ﻿//68.Показать натуральные числа от M до N, N и M заданы
 
+// string A(int M, int N)
+// {
+//     if (M > N) return $" {( M + A(M - 1,N))} ";
+//     else return $" {N}";
+// }
+
+// Console.WriteLine(A(4, 3));
+
+
+
 string A(int M, int N)
 {
+    if (M==N) return $" {N}";
     if (M > N) return $" {( M + A(M - 1,N))} ";
-    else return $" {N}";
+    else return $" {( M + A(M + 1,N))} ";
 }
 
-Console.WriteLine(A(4, 3));
+Console.WriteLine(A(-3, 3)); 
