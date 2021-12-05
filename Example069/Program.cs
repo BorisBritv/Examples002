c4301e36f471bf2ff2e2a946876e42850a384341
BorisBritv <malcoach@mail.ru>
2021-12-05 15:40:16 +0300

    add 069

---
+++ Example069/Program.cs
@@ -0,0 +1,10 @@
+﻿// 69. Найти сумму элементов от M до N, N и M заданы
+
+int A(int M, int N)
+{
+    if (M == N) return N;
+    if (M > N) return M + A(M - 1, N);
+    else return M + A(M + 1, N);
+}
+
+Console.WriteLine(A(3, 3));
